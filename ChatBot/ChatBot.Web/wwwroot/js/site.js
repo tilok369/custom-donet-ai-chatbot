340928e9abcac8b9c25d3bff719263e24f394e43
Tanjeer <tbh.tilok@live.com>
2025-02-27 16:47:00 +0100

    Design finalization

--- ChatBot/ChatBot.Web/wwwroot/js/site.js
+++ ChatBot/ChatBot.Web/wwwroot/js/site.js
@@ -6,9 +6,11 @@
 let chatService = {
     interval: null,
     chatHistory: [],
+    localChatHistory: [],
     init: function () {
         $('#chat-box').keypress(function (event) {
             if (event.keyCode === 13) {
+                event.preventDefault();
                 chatService.initChat();
             }
         });
@@ -17,14 +19,7 @@ let chatService = {
         if (!$('#chat-box').val()) {
             return;
         }
-        let chatText = $('#chat-box').val();
-        $('#chat-box').val(null);
-        $('#chat-window').append(`<div class="row">
-                                    <div class="col-lg-8 col-md-8 col-sm-11 chat-item-container-user">
-                                        ${chatText}
-                                    </div>
-                                </div>`);
-        
+        let chatText = chatService.showUserChat();
         chatService.startThinking();
         
         chatService.chatHistory.push({
@@ -42,16 +37,10 @@ let chatService = {
         })
             .then(response => {
                 console.log('Raw response:', response);
-                return response.json();  // Ensure this is returned
+                return response.json();
             })
             .then(data => {
                 console.log('Success:', data);
-                // $('#chat-window').append(`<div class="row justify-content-end">
-                //                         <div class="col-lg-8 col-md-8 col-sm-11 chat-item-container-ai">
-                //                             ${data.text}
-                //                         </div>
-                //                     </div>`);
-                // $('#chat-window').scrollTop($('#chat-window')[0].scrollHeight);
                 chatService.chatHistory.push({
                     role: 2,
                     text: data.text,
@@ -63,33 +52,12 @@ let chatService = {
             .catch(error => console.error('Error:', error));
     },
 
-    startChat: function () {
-        let chatTitle = $('#chat-window .row .chat-item-container-user:first').text().trim();
-        $('#chat-window').html('');
-        $('#chat-box').val(null);
-        $('#chat-history-container').append(`<li class="list-group-item">${chatTitle}</li>`);
-    },
-    
-    startThinking: function () {
-        let dots = 0;
-        chatService.interval = setInterval(function () {
-            dots = (dots + 1) % 4; // Cycle through 0, 1, 2, 3
-            $("#thinking-lbl").text("Thinking" + ".".repeat(dots));
-        }, 500);
-    },
-    stopThinking: function () {
-        if (chatService.interval) {
-            clearInterval(chatService.interval);
-            $("#thinking-lbl").text(null);
-        }
-    },
-    
     showChat: function (index, text) {
         $('#chat-window').append(`<div class="row justify-content-end">
                                         <div id="item-${index}" class="col-lg-8 col-md-8 col-sm-11 chat-item-container-ai">
                                         </div>
                                     </div>`);
-        
+
         let words = text.split(' ');
         let showLength = 1;
         let interval =setInterval(function () {
@@ -107,5 +75,58 @@ let chatService = {
                 $('#chat-window').scrollTop($('#chat-window')[0].scrollHeight);
             }
         }, 50);
+    },
+
+    showUserChat: function () {
+        let chatText = $('#chat-box').val();
+        $('#chat-box').val('');
+        $('#chat-window').append(`<div class="row">
+                                    <div class="col-lg-8 col-md-8 col-sm-11 chat-item-container-user">
+                                        ${chatText}
+                                    </div>
+                                </div>`);
+
+        $('#chat-window').scrollTop($('#chat-window')[0].scrollHeight);
+        return chatText;
+    },
+
+    clearChat: function () {
+        chatService.chatHistory= [];
+        $('#chat-window').html('');
+        $('#chat-box').val('');
+    },
+    
+    preserveChatHistory: function () {
+        chatService.localChatHistory.push({
+            text: $('#chat-window').html()
+        });
+        
+        return chatService.localChatHistory.length - 1;
+    },
+
+    ShowChatHistory: function (historyIndex) {
+        chatService.clearChat();
+        $('#chat-window').html(chatService.localChatHistory[historyIndex].text);
+    },
+
+    startNewChat: function () {
+        let chatTitle = $('#chat-window .row .chat-item-container-user:first').text().trim();
+        var chatHistoryIndex = chatService.preserveChatHistory();
+        chatService.clearChat();
+        $('#chat-history-container').append(`<li onclick="chatService.ShowChatHistory(${chatHistoryIndex})" class="list-group-item chat-history-item">${chatTitle}</li>`);
+    },
+    
+    startThinking: function () {
+        let dots = 0;
+        chatService.interval = setInterval(function () {
+            dots = (dots + 1) % 4; // Cycle through 0, 1, 2, 3
+            $("#thinking-lbl").text("Thinking" + ".".repeat(dots));
+        }, 500);
+    },
+    stopThinking: function () {
+        if (chatService.interval) {
+            clearInterval(chatService.interval);
+            $("#thinking-lbl").text(null);
+        }
     }
 };
